ae565bcfb9de92e6ddeb75418ea010bfcf8d404e
ashrith1357 <ramu.siemens@gmail.com>
2018-10-09 07:11:07 -0700

    Update terraform_validate_with_variables.sh

--- terraform_validate_with_variables.sh
+++ terraform_validate_with_variables.sh
@@ -5,22 +5,12 @@ declare -a paths
 index=0
 
 for file_with_path in "$@"; do
-  file_with_path="${file_with_path// /__REPLACED__SPACE__}"
-
   paths[index]=$(dirname "$file_with_path")
-  (( "index+=1" ))
+  let "index+=1"
 done
 
 for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
-  path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
-
   pushd "$path_uniq" > /dev/null
-  if [[ -n "$(find . -maxdepth 1 -name '*.tf' -print -quit)" ]] ; then
-    if ! terraform validate -check-variables=true ; then
-      echo
-      echo "Failed path: $path_uniq"
-      echo "================================"
-    fi
-  fi
+  terraform validate -check-variables=false
   popd > /dev/null
 done
